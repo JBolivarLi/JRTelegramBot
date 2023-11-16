e393d3b226602f4ccd493f8aaa989590f9ebaa90
JBolivarLi <147824016+JBolivarLi@users.noreply.github.com>
2023-11-16 13:59:13 +0300

    JRTB-2: added stub telegram bot ti the project (#16)
    * JRTB-2: added stub telegram bot ti the project
    
    * updated project version and added to RELEASE_NOTES

--- src/main/java/com/github/JBolivarLi/javarushtelegrambot/bot/JavarushTelegrambotApplication.java
+++ src/main/java/com/github/JBolivarLi/javarushtelegrambot/bot/JavarushTelegrambotApplication.java
@@ -1,9 +1,14 @@
-package com.github.JBolivarLi.javarushtelegrambot;
+package com.github.JBolivarLi.javarushtelegrambot.bot;
 
+import com.github.JBolivarLi.javarushtelegrambot.bot.bot.JavarushTelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
 
 @SpringBootApplication
+
 public class JavarushTelegrambotApplication {
 
 	public static void main(String[] args) {
