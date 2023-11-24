f8fe6d311f34eb305427ba5eeb41e606b0cf17af
JBolivarLi <litvinovivanvadimovich@gmail.com>
2023-11-24 10:14:14 +0300

    updated project version and added to RELEASE_NOTES

--- src/test/java/com/github/JBolivarLi/javarushtelegrambot/JavarushTelegrambotApplicationTests.java
+++ src/test/java/com/github/JBolivarLi/javarushtelegrambot/JavarushTelegrambotApplicationTests.java
@@ -2,12 +2,12 @@ package com.github.JBolivarLi.javarushtelegrambot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JavarushTelegrambotApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//
+////@SpringBootTest
+//class JavarushTelegrambotApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
